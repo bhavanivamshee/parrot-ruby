02ef8e26a25e1a5ce5a7aa44e1ed9cbbe66595a7
Bhavani <bhavanivamshee@gmail.com>
2021-01-21 13:09:09 -0600

    commit tree 7682e644dc4e00c3d6e376d4574a84a30b30e873

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(default_phrase = "Squawk!")
+    puts "#{default_phrase}"
+    "#{default_phrase}"
+end
+
+parrot
+
+parrot("Pretty bird!")
+
